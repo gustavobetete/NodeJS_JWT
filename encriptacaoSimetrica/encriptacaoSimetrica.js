de29b85913517dc27f17478710ba79403deff558
gustavobetete <gustavobetete@hotmail.com>
2023-08-02 15:43:42 -0300

    encriptacao simetrica

---
+++ encriptacaoSimetrica/encriptacaoSimetrica.js
@@ -0,0 +1,20 @@
+import { createCipheriv, createDecipheriv, randomBytes } from 'crypto';
+
+const mensagem = 'Demonstração do curso de criptografia simétrica';
+const chave = randomBytes(32);
+const iv = randomBytes(16);
+
+const cifra = createCipheriv('aes-256-gcm', chave, iv);
+
+let mensagemCifrada = cifra.update(mensagem, 'utf8', 'hex');
+mensagemCifrada += cifra.final('hex');
+
+console.log(mensagemCifrada);
+
+// Transmissão ---------------- chave, iv, mensagemCifrada
+
+const decifra = createDecipheriv('aes-256-gcm', chave, iv);
+
+const mensagemDecrifrada = decifra.update(mensagemCifrada, 'hex', 'utf8');
+
+console.log(mensagemDecrifrada);
